d775dad74588576019f53595a65657ab82c01514
Jonathan Kyule <jonathankyule2@gmail.com>
2024-03-06 10:49:39 +0300

    views files

---
+++ src/views/TaskCreationView.vue
@@ -0,0 +1,20 @@
+<!-- src/views/TaskCreationView.vue -->
+
+<template>
+    <div>
+      <h1>Task Creation View</h1>
+      <!-- Add your task creation form and logic here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TaskCreationView'
+    // Add your component logic here
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your component-specific styles here */
+  </style>
+  
